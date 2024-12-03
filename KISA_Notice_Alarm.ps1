ff7123aaa8c032c03c79ac2e6aab03580261dbe2
ymiwm <ymiwm0322@gmail.com>
2024-12-03 16:36:20 +0900

    Create KISA_Notice_Alarm.ps1

---
+++ KISA_Notice_Alarm.ps1
@@ -0,0 +1,49 @@
+# ┌────────────────────────────────┐
+# │     KISA_Notice_Alarm V1.0     │
+# │     Author: Alex               │
+# │     Last Modified: 2024-12-03  │
+# └────────────────────────────────┘
+
+# Todo List
+# 1. MessageBox와 Non-Block하게 코드 실행
+# 2. KISA(krcert) 사이트 변동 시 코드 조정 필요 -> Class, Tag로 고정하거나 "공지" 행 제외
+
+$kisa_latest_id=0
+$kisa_cur_id=0
+$kisa_cur_title=""
+$flag=$false
+$count=0
+
+while($true){
+    if($(Get-Date).Second -ne 0){
+        continue
+    }
+    $cur_time=Get-Date
+    $count+=1
+
+    $kisa_html=Invoke-WebRequest "https://krcert.or.kr/kr/bbs/list.do?menuNo=205020&bbsId=B0000133"
+
+    try{
+        $kisa_cur_id=$kisa_html.AllElements | ?{$_.Class -eq "num"} | select -index 1 | select -ExpandProperty InnerText
+        $kisa_cur_title=$kisa_html.AllElements | ?{$_.Class -eq "sbj tal"} | select -index 0 | select -ExpandProperty InnerText
+    }
+    catch{
+        echo "[-] ($($cur_time.ToString("hh:mm")) HTML 파싱 오류 발생.(Count: $count)($_)"
+    }
+
+    if($kisa_latest_id -ne $kisa_cur_id){
+        echo "[+] ($($cur_time.ToString("hh:mm"))) KISA 신규 공지 발생.(Count: $count)"
+        echo "`t($kisa_cur_id) $kisa_cur_title"
+        $kisa_latest_id=$kisa_cur_id
+        $flag=$true
+    }
+    else{
+        echo "[+] ($($cur_time.ToString("hh:mm"))) KISA 신규 공지 없음.(Count: $count)"
+        $flag=$false
+    }
+    
+    if($flag){
+        [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') > $null
+        [System.Windows.Forms.MessageBox]::Show($THIS, "($kisa_cur_id) $kisa_cur_title","KISA 신규 공지 발생") > $null
+    }
+}
